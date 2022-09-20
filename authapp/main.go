679ab27da97731d0985f6a29d61fb0f745097659
M.R. Murazza <muh.rafif.m@gmail.com>
2022-09-20 17:43:41 +0700

    remove unnecessary extra logger middleware

--- authapp/main.go
+++ authapp/main.go
@@ -6,9 +6,6 @@ import (
 	"authapp/pkg/auth"
 	"authapp/pkg/database"
 
-	"log"
-	"time"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,7 +21,7 @@ func main() {
 	// init handler
 	apiHandler := handler.NewApiHandler(itemSvc)
 
-	v1 := r.Group("/api/v1", Logger())
+	v1 := r.Group("/api/v1")
 	{
 		v1.POST("/login", apiHandler.Login)
 		v1.POST("/user", apiHandler.CreateUser)
@@ -39,20 +36,4 @@ func main() {
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
-}
-
-func Logger() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		t := time.Now()
-
-		c.Next()
-
-		// after request
-		latency := time.Since(t)
-		log.Print(latency)
-
-		// access the status we are sending
-		status := c.Writer.Status()
-		log.Println(status)
-	}
 }
